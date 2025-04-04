7d0fb4a4c55e30fc49a9f6b002bb6ed3934c5975
Dhruv Jain <119651242+dhruvjain23@users.noreply.github.com>
2025-04-05 00:46:36 +0530

    update mongourl

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 
 export const connectDB= async ()=>{
 
-    await mongoose.connect('mongodb+srv://djain01d:aRloeBzj3byCMSH0@cluster0.njg29.mongodb.net/FOOD-DELIVERY').then(()=>console.log('DB Connected'));
+    await mongoose.connect('Your-mongoURL').then(()=>console.log('DB Connected'));
 
 }
